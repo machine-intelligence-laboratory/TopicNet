d78d8567a5b29cc0443a1e9baea7d11ad97f1871
Евгения Веселова <xtonev@gmail.com>
2019-11-08 21:09:33 +0300

    setup.py with the latest link

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'topicnet',         # How you named your package folder (MyLib)
   packages = ['topicnet', 'topicnet.cooking_machine', 'topicnet.cooking_machine.models', 'topicnet.cooking_machine.cubes', 'topicnet.viewers'],   # Chose the same as "name"
-  version = '0.3.0',      # Start with a small number and increase it with every change you make
+  version = '0.4.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TopicNet is a module for topic modelling using ARTM algorithm',   # Give a short description about your library
   author = 'Machine Intelligence Laboratory',                   # Type in your name
   author_email = 'alex.goncharov@phystech.edu',      # Type in your E-Mail
   url = 'https://github.com/machine-intelligence-laboratory/TopicNet',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/machine-intelligence-laboratory/TopicNet/archive/v0.3.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/machine-intelligence-laboratory/TopicNet/archive/v0.4.0.tar.gz', 
   keywords = ['ARTM', 'topic', 'modelling', 'visualization'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
@@ -25,7 +25,7 @@ setup(
           'strictyaml'
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Scientific/Engineering :: Artificial Intelligence',
     'Topic :: Scientific/Engineering :: Information Analysis',
