4dbfa7a077a9adc69527f762da1e5c65c518b74b
Vasiliy Alekseev <alvasian@yandex.ru>
2020-02-21 14:20:04 +0300

    fulfil

--- setup.py
+++ setup.py
@@ -1,37 +1,50 @@
 from distutils.core import setup
+
 setup(
-  name = 'topicnet',         # How you named your package folder (MyLib)
-  packages = ['topicnet', 'topicnet.cooking_machine', 'topicnet.cooking_machine.models', 'topicnet.cooking_machine.cubes', 'topicnet.viewers'],   # Chose the same as "name"
-  version = '0.5.0',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TopicNet is a module for topic modelling using ARTM algorithm',   # Give a short description about your library
-  author = 'Machine Intelligence Laboratory',                   # Type in your name
-  author_email = 'alex.goncharov@phystech.edu',      # Type in your E-Mail
-  url = 'https://github.com/machine-intelligence-laboratory/TopicNet',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/machine-intelligence-laboratory/TopicNet/archive/v0.5.0.tar.gz', 
-  keywords = ['ARTM', 'topic', 'modelling', 'visualization'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'numpy',
-          'six',
-          'scipy',
-          'numexpr',
-          'pytest',
-          'pandas',
-          'tqdm',
-          'dask',
-          'scikit_learn',
-          'typing',
-          'ipython',
-          'strictyaml',
-          'plotly',
-          'colorlover',
-      ],
-  classifiers=[
-    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    'Topic :: Scientific/Engineering :: Information Analysis',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3.6',
-  ],
+    name = 'topicnet',
+    packages = [
+        'topicnet',
+        'topicnet.cooking_machine',
+        'topicnet.cooking_machine.cubes',
+        'topicnet.cooking_machine.models',
+        'topicnet.viewers'
+    ],
+    version = '0.5.0',
+    license='MIT',
+    description = 'TopicNet is a module for topic modelling using ARTM algorithm',
+    author = 'Machine Intelligence Laboratory',
+    author_email = 'alex.goncharov@phystech.edu',
+    url = 'https://github.com/machine-intelligence-laboratory/TopicNet',
+    download_url = 'https://github.com/machine-intelligence-laboratory/TopicNet/archive/v0.5.0.tar.gz', 
+    keywords = [
+        'ARTM',
+        'topic modeling',
+        'regularization',
+        'multimodal learning',
+        'document vector representation'
+    ],
+    install_requires=[
+        'colorlover',
+        'dask',
+        'dill',
+        'ipython',
+        'numexpr',
+        'numpy',
+        'pandas',
+        'plotly',
+        'pytest',
+        'scikit_learn',
+        'scipy',
+        'six',
+        'strictyaml',
+        'tqdm',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
